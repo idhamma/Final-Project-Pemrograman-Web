655dd2dafd32917e0cf9f08b50e9b28698e32124
Sleepy <idhammakbar@gmail.com>
2024-05-17 00:15:26 +0700

    [fix] position on page motorcycles

--- resources/views/motorcycles.blade.php
+++ resources/views/motorcycles.blade.php
@@ -13,16 +13,17 @@
     @include('navigation_bar')
 
     <!-- MAIN CONTENT -->
-    <main style="overflow: hidden;">
+    <main>
         <section>
             <div class="textkategori">
                 <h1>Check Out Our Categories!</h1>
             </div>
             <div class="jeniscar">
+            <!-- <div class="cuandprior"> -->
                 <div class="jenis-card">
-                    <h2>Mini Car</h2>
+                    <h2>Motor ngab</h2>
                     <div class="garis"></div>
-                    <img src="../Assets/minicar.png" alt="minicar">
+                    <img src="image/sample/motor.png" alt="minicar">
                     <p>'Mini' cars are the smallest types of cars you can hire. Minis are suitable for three or four
                         people for very short trips. For longer trips, they’re more suitable for one or two people.
                         Minis are often (but not necessarily) hatchbacks.</p>
@@ -55,10 +56,12 @@
                     <p>SUVs (short for sport or suburban utility vehicles) are good-sized vehicles with a rugged look,
                         set higher off the road than normal cars.</p>
 
-                </div>
+                <!-- </div> -->
+            </div>
             </div>
         </section>
     </main>
+
     <!-- FOOTER -->
     @include('footer')
 </body>
